c68c9a8d96cad77ead105817df9d655717747d21
lfr <1733535832@qq.com>
2024-07-11 22:26:05 +0800

    updata  add_lib

--- cmake/build.cmake
+++ cmake/build.cmake
@@ -1,41 +1,40 @@
-function(llcompiler_add_library TargetName LinkLibs SourceFiles)
-    if(NOT ${${SourceFiles}} STREQUAL ${LLCOMPILER_CURRENT_DIR_FILES})
-        set(library_source_files ${${SourceFiles}})
-    else()
-        file(GLOB_RECURSE library_source_files ${CMAKE_CURRENT_SOURCE_DIR}/*.cpp)
-    endif()
-
-    # #---------- compile llcimpiler ----------##
-    if(BUILD_SHARED_LIBS)
-        add_library(${TargetName} SHARED ${library_source_files})
-    else()
-        add_library(${TargetName} STATIC ${library_source_files})
-    endif()
-
-    set_target_properties(${TargetName} PROPERTIES DEBUG_POSTFIX ${CMAKE_DEBUG_POSTFIX})
-    target_include_directories(${TargetName}
-        PRIVATE
-        $<BUILD_INTERFACE:${LLCOMPILER_INCLUDE_DIR}>
-        $<INSTALL_INTERFACE:${LLCOMPILER_INSTALL_DIR}>
-    )
-    target_link_libraries(${TargetName} PUBLIC ${${LinkLibs}})
-endfunction(llcompiler_add_library)
-
-function(add_catch_test)
-    set(options)
-    set(oneValueArgs NAME COST)
-    set(multiValueArgs LABELS DEPENDS REFERENCE_FILES)
-    cmake_parse_arguments(add_catch_test
-        "${options}"
-        "${oneValueArgs}"
-        "${multiValueArgs}"
+function(llcompiler_add_library name)
+    cmake_parse_arguments(ARG
+        ""
+        ""
+        "SRC_FILES;DEPENDS;LINKS"
         ${ARGN}
     )
-    message(STATUS "defining a test ...")
-    message(STATUS " NAME: ${add_catch_test_NAME}")
-    message(STATUS " LABELS: ${add_catch_test_LABELS}")
-    message(STATUS " COST: ${add_catch_test_COST}")
-    message(STATUS " REFERENCE_FILES: ${add_catch_test_REFERENCE_FILES}")
 
-    
+    # #---------- compile lib ----------##
+    if(ARG_SRC_FILES)
+        if(BUILD_SHARED_LIBS)
+            add_library(${name} SHARED ${ARG_SRC_FILES})
+        else()
+            add_library(${name} STATIC ${ARG_SRC_FILES})
+        endif()
+
+        target_include_directories(${name}
+            PRIVATE
+            $<BUILD_INTERFACE:${LLCOMPILER_INCLUDE_DIR}>
+            $<INSTALL_INTERFACE:${LLCOMPILER_INSTALL_DIR}>
+        )
+
+    # #---------- INTERFACE lib ----------##
+    else()
+        add_library(${name} INTERFACE)
+        target_include_directories(${name}
+            INTERFACE
+            $<BUILD_INTERFACE:${LLCOMPILER_INCLUDE_DIR}>
+            $<INSTALL_INTERFACE:${LLCOMPILER_INSTALL_DIR}>
+        )
+    endif()
+
+    set_target_properties(${name} PROPERTIES DEBUG_POSTFIX ${CMAKE_DEBUG_POSTFIX})
+
+    if(ARG_DEPENDS)
+        add_dependencies(${name} ${ARG_DEPENDS})
+    endif()
+
+    target_link_libraries(${name} PUBLIC ${ARG_LINKS})
 endfunction()
