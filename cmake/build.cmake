41b7300adca9baa6fbae3b7cd368d7f37d64f29c
lfr <1733535832@qq.com>
2024-07-12 20:47:26 +0800

    cmake refine

--- cmake/build.cmake
+++ cmake/build.cmake
@@ -7,28 +7,21 @@ function(llcompiler_add_library name)
     )
 
     # #---------- compile lib ----------##
-    if(ARG_SRC_FILES)
-        if(BUILD_SHARED_LIBS)
-            add_library(${name} SHARED ${ARG_SRC_FILES})
-        else()
-            add_library(${name} STATIC ${ARG_SRC_FILES})
-        endif()
-
-        target_include_directories(${name}
-            PRIVATE
-            $<BUILD_INTERFACE:${LLCOMPILER_INCLUDE_DIR}>
-            $<INSTALL_INTERFACE:${LLCOMPILER_INSTALL_DIR}>
-        )
-
-    # #---------- INTERFACE lib ----------##
-    else()
-        add_library(${name} INTERFACE)
-        target_include_directories(${name}
-            INTERFACE
-            $<BUILD_INTERFACE:${LLCOMPILER_INCLUDE_DIR}>
-            $<INSTALL_INTERFACE:${LLCOMPILER_INSTALL_DIR}>
-        )
+    if(NOT ARG_SRC_FILES)
+        message(FATAL_ERROR "No source files specified for library ${name}")
     endif()
+
+    if(BUILD_SHARED_LIBS)
+        add_library(${name} SHARED ${ARG_SRC_FILES})
+    else()
+        add_library(${name} STATIC ${ARG_SRC_FILES})
+    endif()
+
+    target_include_directories(${name}
+        PRIVATE
+        $<BUILD_INTERFACE:${LLCOMPILER_INCLUDE_DIR}>
+        $<INSTALL_INTERFACE:${LLCOMPILER_INSTALL_DIR}>
+    )
 
     set_target_properties(${name} PROPERTIES DEBUG_POSTFIX ${CMAKE_DEBUG_POSTFIX})
 
