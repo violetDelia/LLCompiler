d6ea96d990eea709c605c15d4a0020cc9fce9003
lfr <1733535832@qq.com>
2024-08-04 23:11:26 +0800

    failed to legalize operation 'builtin.module'

--- tools/llc-opt/llc-opt.cpp
+++ tools/llc-opt/llc-opt.cpp
@@ -11,20 +11,28 @@
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
+#include "llcompiler/Conversion/LLHToTosa/LLHToTosa.h"
 #include "llcompiler/Conversion/Passes.h"
 #include "llcompiler/Dialect/LLH/IR/LLHDialect.h"
+#include "mlir/Dialect/Func/Extensions/InlinerExtension.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/Tosa/IR/TosaOps.h"
+#include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/DialectRegistry.h"
+#include "mlir/Interfaces/CastInterfaces.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
-
-
+#include "mlir/Transforms/Passes.h"
+//--dump-pass-pipeline --inline --convert-llh-to-tosa
 int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
   registry.insert<mlir::func::FuncDialect>();
   registry.insert<mlir::llh::LLHDialect>();
   registry.insert<mlir::tosa ::TosaDialect>();
+  mlir::builtin::registerCastOpInterfaceExternalModels(registry);
+  mlir::func::registerInlinerExtension(registry);
+  mlir::registerInlinerPass();
+  mlir::registerCanonicalizer();
   mlir::registerConvertLLHToTosa();
   return mlir::asMainReturnCode(
       mlir::MlirOptMain(argc, argv, "llc-opt", registry));
