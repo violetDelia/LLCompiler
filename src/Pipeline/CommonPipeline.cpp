b7cfe8392d78aea1628098e53e9a118b88c71595
lfr <1733535832@qq.com>
2024-08-15 01:29:23 +0800

    add print op

--- src/Pipeline/CommonPipeline.cpp
+++ src/Pipeline/CommonPipeline.cpp
@@ -12,41 +12,51 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-#include "llcompiler/Conversion/LLHToTosa/LLHToTosa.h"
 #include "llcompiler/Pipeline/CommonPipeline.h"
+
+#include "llcompiler/Conversion/LLHToTosa/LLHToTosa.h"
+#include "mlir/Dialect/Tosa/Transforms/Passes.h"
 #include "mlir/Pass/PassOptions.h"
 #include "mlir/Pass/PassRegistry.h"
 #include "mlir/Transforms/Passes.h"
 #include "mlir/pass/PassManager.h"
-#include "mlir/Dialect/Tosa/Transforms/Passes.h"
 
 namespace llc::pipleline {
 
 struct CommonPipelineOptions
-    : public mlir::PassPipelineOptions<CommonPipelineOptions> {};
+    : public mlir::PassPipelineOptions<CommonPipelineOptions> {
+  Option<bool> printOpGraph{*this, "print-op-graph",
+                            llvm::cl::desc("use PrintOpGraphPass.")};
+};
+
+void buildCommonPipelineTosaOpt(::mlir::OpPassManager &pm,
+                                const CommonPipelineOptions &options) {
+  pm.addPass(mlir::createCanonicalizerPass());          // 规范化
+  pm.addPass(mlir::tosa::createTosaInferShapesPass());  // 形状推导
+  pm.addPass(mlir::tosa::createTosaMakeBroadcastablePass());//规范算子广播形状
+}
 
 void buildCommonPipeline(::mlir::OpPassManager &pm,
                          const CommonPipelineOptions &options) {
   pm.addPass(::mlir::createInlinerPass());       // 内联
   pm.addPass(::mlir::createConvertLLHToTosa());  // LLH lowing to tosa
-  pm.addPass(mlir::createSCCPPass());            // 稀疏常量条件传播
-  pm.addPass(mlir::tosa::createTosaInferShapesPass());  // 形状推导
-  pm.addPass(mlir::createCanonicalizerPass());          // 规范化
-
-  // general pass
-  {
-    pm.addPass(mlir::createCompositeFixedPointPass());
-    pm.addPass(mlir::createSROA());
-    pm.addPass(mlir::createTopologicalSortPass());
+  buildCommonPipelineTosaOpt(pm, options);
+  if (options.printOpGraph) {
     pm.addPass(mlir::createPrintOpGraphPass());
-
-    pm.addPass(mlir::createMem2Reg());
-  }
-  pm.addPass(
-      mlir::createControlFlowSinkPass());  // 将算子下沉到使用它的控制块中
+  }  // 输出Op graph
   pm.addPass(mlir::createCSEPass());               // 公共表达式消除
   pm.addPass(mlir::createRemoveDeadValuesPass());  // 死代码消除
   pm.addPass(mlir::createSymbolDCEPass());         // 死符号消除
+  if (false) {
+    pm.addPass(mlir::createSCCPPass());  // 稀疏常量条件传播
+    pm.addPass(mlir::createCompositeFixedPointPass());
+    pm.addPass(mlir::createSROA());
+    pm.addPass(mlir::createTopologicalSortPass());
+
+    pm.addPass(mlir::createMem2Reg());
+    pm.addPass(
+        mlir::createControlFlowSinkPass());  // 将算子下沉到使用它的控制块中
+  }
 }
 
 void registerCommonPipeline() {
